20e3bd8cc5df16067036fe33954525c605c30c68
tmd45 <tmd45@otegami.jpn.org>
2019-09-22 16:04:57 +0900

    Save the device token in the Cloud Firestore

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -113,7 +113,19 @@ function saveImageMessage(file) {
 
 // Saves the messaging device token to the datastore.
 function saveMessagingDeviceToken() {
-  // TODO 10: Save the device token in the realtime datastore
+  firebase.messaging().getToken().then(function (currentToken) {
+    if (currentToken) {
+      console.log('Got FCM device token:', currentToken);
+      // Saving the Device Token to the datastore.
+      firebase.firestore().collection('fcmTokens').doc(currentToken)
+        .set({ uid: firebase.auth().currentUser.uid });
+    } else {
+      // Need to request permissions to show notifications.
+      requestNotificationsPermissions();
+    }
+  }).catch(function (error) {
+    console.error('Unable to get messaging token.', error);
+  });
 }
 
 // Requests permissions to show notifications.
